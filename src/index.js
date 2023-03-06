06cb58e4ed994737f0b35d7f776d748c5ff4d310
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2023-03-06 22:25:09 +0100

    Udated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<StrictMode>
-  <App />
-</StrictMode>);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
