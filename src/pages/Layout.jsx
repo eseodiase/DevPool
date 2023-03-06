8ce6beacbdd67fd2ab5c0189a92aabcbf9f19537
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2023-03-06 23:14:08 +0100

    Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -10,10 +10,16 @@ const Layout = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/intern">Internship Jobs</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/front-end">Front-end Developer Jobs</Link>
+          </li>
+          <li>
+            <Link to="/full-stack">Full-stack Developer Jobs</Link>
+          </li>
+          <li>
+            <Link to="/engineer">Software Engineer Jobs</Link>
           </li>
         </ul>
       </nav>
