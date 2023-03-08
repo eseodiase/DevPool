2590192a74822380325f118926232b28b4f48e98
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2023-03-08 19:43:36 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import CVMaker from "./pages/CVMaker";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="cvmaker" element={<CVMaker />}>
         </Route>
       </Routes>
     </BrowserRouter>
